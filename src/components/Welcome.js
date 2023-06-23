5e906936aa1641b11bcb4ef05c430bd6818a8083
chinni00 <chinni0437@gmail>
2023-06-23 21:43:23 +0530

    mail verification functionality added

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { json, useNavigate } from 'react-router-dom'
 
 const Welcome = () => {
   const token = localStorage.getItem('token');
@@ -22,11 +22,24 @@ const Welcome = () => {
   const switchToUpdatePage=()=>{
      navigate('/update-profile')
   }
+  const emailVerificationHandler = ()=>{
+          fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDeEdrVt4u11fGigu620gWbNo_fW39vd7s',{
+            method:'POST',
+            body:JSON.stringify({
+              requestType:"VERIFY_EMAIL",
+              idToken:token,
+            }),
+            headers:{
+              'Content-Type':'application/json'
+            }
+          }).then(res=>res.json()).then(data=>console.log(data))
+  }
   return (
     <div>
         <center>
             <h1>Welcome to expense tracker</h1>
-            <span><i>Your Profile is Incompleted <a href='#' onClick={switchToUpdatePage} >Complete Now</a></i></span>
+            <span><i>Your Profile is Incompleted <a href='#' onClick={switchToUpdatePage} >Complete Now</a></i></span><br />
+          You have to verify your email to maintain ur account long   <button onClick={emailVerificationHandler} className='btn  btn-secondary'>Verify Email</button>
         </center>
        
     </div>
