3a1e8d6e55d5e6dfdc2ad51273f52bfc36b723e6
chinni00 <chinni0437@gmail>
2023-06-26 02:35:18 +0530

    edit and deleted added successfully

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,23 +1,37 @@
-import React from 'react'
+import React from "react";
 
-const ExpenseList = ({value}) => {
+const ExpenseList = ({value,onDelete,onEdit}) => {
+console.log('value',value[0])
+let expense = Object.entries(value[0])
+console.log(' exp',expense)
+
     
   return (
-    <div className='card m-5 p-4'>
-        <center>
+    <div className="card m-5 p-4">
+      <center>
         <h2>Expense List</h2>
-        <h4 className='w-50 d-flex justify-content-around '> <i>Spent Money</i> <i>Category</i> <i>Description</i></h4>
-        {value.map((item)=>
-            <div className='border border-2 border-black p-2 w-50 d-flex justify-content-around mt-3'style={{fontWeight:400}}>
-            <span className='ms-5'  >$. {item.price}</span>
-            <span className='ms-5' >{item.category}</span>
-            <span className='ms-5' >{item.description}</span>
-        </div>
-           
-        )}
-        </center>
-    </div>
-  )
-}
+        <h4 className=" d-flex justify-content-around" style={{width:'60%',marginLeft:'-160px'}}>
+          <i>Spent Money</i> <i style={{marginLeft:'-60px'}}>Category</i> <i style={{marginLeft:'-50px'}}>Description</i>
+        </h4>
+        {expense.map((item) => (
+             <div
+             key={item[0]}
+             className="border border-2 border-black p-2 w-75 d-flex justify-content-around mt-3"
+             style={{ fontWeight: 400 }}
+           >
+             <span className="ms-5 ">$. {item[1].price}</span>
+             <span className="ms-5">{item[1].category}</span>
+             <span className="ms-5">{item[1].description}</span>
 
-export default ExpenseList
+             <button className="btn btn-danger" onClick={()=>{onDelete(item[0])}}>Delete</button>
+             <button className="btn btn-success opacity-75" onClick={()=>{onEdit(item[0])}}>Edit</button>
+           </div>
+         
+        ))}
+        
+      </center>
+    </div>
+  );
+};
+
+export default ExpenseList;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,6 +4,8 @@ import Navbar from "./Navbar";
 import ExpenseList from "./ExpenseList";
 
 const Welcome = () => {
+  const [id,setId]=useState('')
+  const [isEdit,setIsEdit] = useState(false)
   const [expenses,setExpenses] = useState([])
    const moneyInputRef = useRef()
    const descriptionInputRef = useRef()
@@ -13,16 +15,15 @@ const Welcome = () => {
     fetch('https://expense-tracker-auth-b79c5-default-rtdb.firebaseio.com/expenses.json')
       .then(res => res.json())
       .then(data => {
-        const fetchedExpenses = Object.values(data);
-        setExpenses(fetchedExpenses);
+         setExpenses([data])
       })
       .catch(err => console.log(err));
   };
   
 
   const submitHandler = (event) => {
-    event.preventDefault();
-    const enteredMoney = moneyInputRef.current.value;
+     event.preventDefault()
+      const enteredMoney = moneyInputRef.current.value;
     const enteredDescription = descriptionInputRef.current.value;
     const enteredCategory = categoryInputRef.current.value;
     let obj = {
@@ -30,7 +31,11 @@ const Welcome = () => {
       description: enteredDescription,
       category: enteredCategory
     };
-  
+     
+    if(!isEdit){
+     
+
+   
     fetch('https://expense-tracker-auth-b79c5-default-rtdb.firebaseio.com/expenses.json', {
       method: 'POST',
       body: JSON.stringify(obj),
@@ -46,13 +51,66 @@ const Welcome = () => {
         fetchingHandler(); // Fetch expenses again after the new expense is saved
       })
       .catch(err => console.log(err));
+    }else{
+      
+      
+      if (!enteredMoney || !enteredDescription || !enteredCategory) {
+        alert("Please fill in all the fields");
+        return;
+      }
+      
+      const obj = {
+        price: enteredMoney,
+        description: enteredDescription,
+        category: enteredCategory
+      };
+    
+      fetch(`https://expense-tracker-auth-b79c5-default-rtdb.firebaseio.com/expenses/${id}.json`, {
+        method: 'PUT',
+        body: JSON.stringify(obj),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(res => res.json())
+        .then(() => {
+          moneyInputRef.current.value = '';
+          descriptionInputRef.current.value = '';
+          categoryInputRef.current.value = '';
+          fetchingHandler(); // Fetch expenses again after the expense is edited
+          alert('Edited successfully');
+        })
+        .catch(err => console.log(err));
+        setIsEdit(false)
+    }
+  };
+
+  const deleteHandler=(id)=>{
+   console.log(id)
+   fetch(`https://expense-tracker-auth-b79c5-default-rtdb.firebaseio.com/expenses/${id}.json`,{
+    method:'DELETE'
+   }).then(res=>res.json()).then(data=>fetchingHandler())
+   
+  }
+
+  const editHandler = (id) => {
+    setIsEdit(true)
+    submitHandler(id)
+    alert('edit mode is on')
+    moneyInputRef.current.focus()
   };
   
+ const idHandler=(id)=>{
+setId(id)
+setIsEdit(true)
+ }
   
+   
   useEffect(()=>{
    fetchingHandler()
   },[])
 
+  console.log(expenses)
   return (
     <div>
       <Navbar />
@@ -84,7 +142,7 @@ const Welcome = () => {
             <div className="form-group m-2">
               <label htmlFor="exampleFormControlSelect1" className=" float-start">Category :</label>
               <select ref={categoryInputRef} className="form-control" id="exampleFormControlSelect1">
-                <option selected  value={'none'} >Select Category</option>
+                <option defaultValue={'none'}   >Select Category</option>
                 <option>Food</option>
                 <option>Petrol</option>
                 <option>Entertainment</option>
@@ -92,12 +150,12 @@ const Welcome = () => {
               </select>
             </div>
             <button type="submit" className="btn btn-primary">
-              Submit
+             { isEdit? 'Edit':'Submit'}
             </button>
           </form>
         </div>
       </center>
-     {expenses.length>0 && <ExpenseList  value={expenses} />}
+     {expenses.length>0 && <ExpenseList  value={expenses} onDelete={deleteHandler} onEdit={idHandler} />}
 
     </div>
   );
